6a9e51d27ab2f6815fabf78c5f9a4aa43cbba0cc
Павел Повстяной <srakakaka001@gmail.com>
2023-11-15 14:32:24 +0400

    Добавьте файлы проекта.

---
+++ Avto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class Avto
+    {
+        public string Br, 
+            Co;
+        public double Pow;
+        private double Sp;
+
+        public double Go(double vr, double rs)
+        {
+            return Sp = rs / vr;
+        }
+
+
+
+
+
+        public void AvtoS()
+        {
+                Console.WriteLine("\nХарактеристики:");
+
+            Console.WriteLine("Скорость: " + Sp);
+
+                    Console.WriteLine("Марка: " + Br);
+            Console.WriteLine("Цвет: " + Co);
+                                    Console.WriteLine("Мощность: " + Pow);
+        
+
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double S, T;
+            Avto c = new Avto();
+
+            Console.Write("Введите марку автомобиля: ");
+            c.Br = Console.ReadLine();
+
+            Console.Write("Введите цвет автомобиля: ");
+            c.Co = Console.ReadLine();
+            
+                    Console.Write("Введите мощность автомобиля: ");
+            c.Pow = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите расстояние, котрое необходимо проехать (км): ");
+                        S = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите время, за которое необходимо проехать указанное расстояние (ч): ");
+            T = Convert.ToDouble(Console.ReadLine());
+
+            c.Go(T, S);
+            c.AvtoS();
+        }
+    }
+}
